45739d57b5e46468133c7597c9c767da15aa4d68
Yang Yang <geraint0923@gmail.com>
2014-02-06 19:23:47 +0800

    Before big changes

--- nginx-tools/ip-positioning.rb
+++ nginx-tools/ip-positioning.rb
@@ -7,6 +7,7 @@ INPUT_FILE = "ip.count"
 #API_URL = "http://ip-api.com/json/"
 #API_URL = "http://freegeoip.net/json/"
 API_URL = "http://www.telize.com/geoip/"
+TAGS = ["ISP", "Country", "City", "Region"]
 
 def locate_ip(ip)
     $stderr.puts API_URL + ip
@@ -18,7 +19,7 @@ res = Hash.new
 file.each_line do |line|
     sl = line.split
     val = Hash.new
-    val[:count] = sl[1]
+    val["count"] = sl[1]
     vv = locate_ip sl[0]
     vv.each do |kk, vv|
 	val[kk] = vv
@@ -28,5 +29,10 @@ end
 
 #just for test
 res.each do |key, val|
-    puts "#{key}---#{val}"
+    printf("%16s%6d", key, val["count"])
+    TAGS.each do |v|
+	printf("%40s", val[v.downcase].to_s)
+	puts val[v.downcase].to_s.length
+    end
+    puts
 end
