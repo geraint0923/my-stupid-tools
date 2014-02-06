6160b8d3b8d8261fe33fd3363d0e972fd8a2ef36
Yang Yang <geraint0923@gmail.com>
2014-02-06 18:53:28 +0800

    Add location look up function

--- nginx-tools/ip-positioning.rb
+++ nginx-tools/ip-positioning.rb
@@ -1,10 +1,19 @@
 #/usr/bin/env ruby
 
+require 'net/http'
+require 'json'
+
 INPUT_FILE = "ip.count"
+#API_URL = "http://ip-api.com/json/"
+#API_URL = "http://freegeoip.net/json/"
+API_URL = "http://www.telize.com/geoip/"
 
 def locate_ip(ip)
 #TODO should add ip look up code here
-    puts ip
+#    puts ip
+#    JSON.parse(Net::HTTP.get(URI(API_URL + ip)))
+    puts API_URL + ip
+    JSON.parse(Net::HTTP.get(URI(API_URL + ip)))
 end
 
 file = File.open(INPUT_FILE)
@@ -13,6 +22,10 @@ file.each_line do |line|
     sl = line.split
     val = Hash.new
     val[:count] = sl[1]
+    vv = locate_ip sl[0]
+    vv.each do |kk, vv|
+	val[kk] = vv
+    end
     res[sl[0]] = val
 end
 
