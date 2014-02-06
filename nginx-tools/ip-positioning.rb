e9c7d2ece94ffad4981c3a3faaf2cc2443ae6406
Yang Yang <geraint0923@gmail.com>
2014-02-06 19:53:07 +0800

    Update the code for more beautiful print

--- nginx-tools/ip-positioning.rb
+++ nginx-tools/ip-positioning.rb
@@ -8,9 +8,11 @@ INPUT_FILE = "ip.count"
 #API_URL = "http://freegeoip.net/json/"
 API_URL = "http://www.telize.com/geoip/"
 TAGS = ["ISP", "Country", "City", "Region"]
+TAGS_LENGTH = Hash.new
+CUR_INDEX = 0
 
 def locate_ip(ip)
-    $stderr.puts API_URL + ip
+    $stderr.puts CUR_INDEX.to_s + ": " + API_URL + ip
     JSON.parse(Net::HTTP.get(URI(API_URL + ip)))
 end
 
@@ -21,18 +23,31 @@ file.each_line do |line|
     val = Hash.new
     val["count"] = sl[1]
     vv = locate_ip sl[0]
+    CUR_INDEX += 1
     vv.each do |kk, vv|
 	val[kk] = vv
+	if TAGS_LENGTH.include?(kk)
+	    TAGS_LENGTH[kk] = vv.to_s.length if TAGS_LENGTH[kk] < vv.to_s.length
+	else
+	    TAGS_LENGTH[kk] = vv.to_s.length
+	end
     end
     res[sl[0]] = val
 end
 
-#just for test
+printf("%16s%12s", "IP", "Count")
+TAGS.each do |k|
+    kk = k.downcase
+    TAGS_LENGTH[kk] = 12 if TAGS_LENGTH[kk] < 12
+    TAGS_LENGTH[kk] = '%' + (TAGS_LENGTH[kk] + 5).to_s + 's'
+    printf(TAGS_LENGTH[kk], k)
+end
+puts
+
 res.each do |key, val|
-    printf("%16s%6d", key, val["count"])
+    printf("%16s%12d", key, val["count"])
     TAGS.each do |v|
-	printf("%40s", val[v.downcase].to_s)
-	puts val[v.downcase].to_s.length
+	printf(TAGS_LENGTH[v.downcase], val[v.downcase].to_s)
     end
     puts
 end
