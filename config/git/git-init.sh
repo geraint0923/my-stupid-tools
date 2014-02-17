38b893190c9d840eb0da1473b99ec76f9227faae
Yang Yang <geraint0923@gmail.com>
2014-02-18 03:45:55 +0800

    Add git config code

---
+++ config/git/git-init.sh
@@ -0,0 +1,7 @@
+git config --global user.email "geraint0923@gmail.com"
+git config --global user.name "Yang Yang"        
+git config --global core.editor "vim"              
+git config --global color.status auto              
+git config --global color.diff auto                
+git config --global color.branch auto              
+git config --global color.interactive auto         
